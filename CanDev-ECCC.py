8b36db3d01e043fb14d73beab89c5fcf2b1c8781
NaderE <nelgh048@uottawa.ca>
2018-10-20 23:57:55 -0400

    added verification function

---
+++ CanDev-ECCC.py
@@ -0,0 +1,63 @@
+def flagger (data):
+
+    #data = "2012,AO,Artisanal opportunities,future,0,Global average,80.74,"
+    #data[r][c]
+    #counts
+
+    scenerioCounter=0
+    goalsCounter=0
+    longGoalCounter=0
+    dimensionCounter=0
+    regionIDCounter=0
+    region_nameCounter=0
+    valueCounter=0
+
+
+    for i in range(len(data)):
+
+
+        if (not(data[i][0].isdigit())):
+            scenerioCounter=scenerioCounter+1
+
+            #print("Scenerio: " + str(data[i][0]))
+
+        if (not(data[i][1].isalpha())):
+            goalsCounter=goalsCounter+1
+
+            #print("goals: " + str(data[i][1]))
+
+        if (not(data[i][2].replace(" ","").replace("&","").replace("(","").replace(")","").isalpha())):
+            longGoalCounter=longGoalCounter+1
+
+            #print("long goal: " + str(data[i][2]))
+
+        if (not(data[i][3].isalpha())):
+            dimensionCounter=dimensionCounter+1
+
+            #print("dimension " + str(data[i][3]))
+
+        if (not(data[i][4].isdigit())):
+            regionIDCounter=regionIDCounter+1
+
+            #print("region ID: " + str(data[i][4]))
+
+        if (not(data[i][5].replace(" ","").replace("_","").replace("(","").replace(")","").replace("-","").isalpha())):
+            region_nameCounter=region_nameCounter+1
+
+            #print("region name: " + str(data[i][5]))
+
+        if (not(data[i][6].replace(".","").replace("-","").isdigit())):
+            valueCounter=valueCounter+1
+
+            #print("value: " + str(data[i][6]))
+
+    return scenerioCounter, goalsCounter, longGoalCounter, dimensionCounter, regionIDCounter, region_nameCounter, valueCounter
+
+dataset = open('dataTest.txt')
+lines = dataset.readlines()
+row = []
+for line in lines:
+
+    row.append(line.strip().split(","))
+
+print(flagger(row))
